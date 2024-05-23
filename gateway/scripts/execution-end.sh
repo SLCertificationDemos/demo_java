6cf99c246f2b6604019fcb49d91eb88919edfc18
sharkness <samantha.gray@sealights.io>
2024-05-23 12:05:41 -0500

    Removes execution start/end scripts from Gateway

--- gateway/scripts/execution-end.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## The variables are set in the docker-compose file. You can run in as well calling the
-## docker run --env-file=FILE ...
-# SL_TOKEN=token
-# SL_LAB_ID=a
-# SL_BRANCH_NAME=c
-
-java -jar sl-test-listener.jar end -token $SL_TOKEN -labid "$SL_LAB_ID"
--- gateway/scripts/execution-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## The variables are set in the docker-compose file. You can run in as well calling the
-## docker run --env-file=FILE ...
-# SL_TOKEN=token
-# SL_LAB_ID=a
-# SL_BRANCH_NAME=c
-
-java -jar sl-test-listener.jar start -token $SL_TOKEN -testStage $1 -labid "$SL_LAB_ID"
