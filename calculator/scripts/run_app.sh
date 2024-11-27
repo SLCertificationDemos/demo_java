3a2293a724f173948206aed558662f1c4a25e27b
sharkness <samantha.gray@sealights.io>
2024-11-27 17:29:44 -0600

    Add SL params to calculator/run_app.sh

--- calculator/scripts/run_app.sh
+++ calculator/scripts/run_app.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-java -jar $1
+DEBUG=-Dsl.httpDebugLog=yes
+LOGGING=-Dsl.log.toConsole=true
+PACKAGES="i0.sealights.demo.calculator*"
+java -Dsl.token="$SL_TOKEN" \
+    -Dsl.labId="$SL_LAB_ID" \
+    -Dsl.appName="$CALC_APPNAME" \
+    -Dsl.branchName="$CALC_BRANCH_NAME" \
+    -Dsl.buildName="$CALC_BUILD_NAME" \
+    -Dsl.includes="$PACKAGES" \
+    -javaagent:./sl-cd-agent.jar \
+    -jar $1
